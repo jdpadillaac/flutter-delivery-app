62ac93c1eff0e6c3bee3d87cf34a2823b3c898ff
Jonatan Padilla <jonathan962911@gmail.com>
2020-11-03 11:52:13 -0500

    :heavy_check_mark:

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'DeliveryApp',
-      theme: AppTheme().dark(),
+      theme: AppTheme().light(),
       debugShowCheckedModeBanner: false,
       home: MainPage(),
       routes: appRoutes,
